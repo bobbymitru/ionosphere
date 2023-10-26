997d6dc045f0a69a1c46af9bfbf230657b5b73a9
Robert Mitru <robert.mitru.business@gmail.com>
2023-10-26 14:42:08 +0300

    Added the project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The data collected by a radar system in Goose Bay, Labrador',
+    author='Robert Mitru',
+    license='MIT',
+)
